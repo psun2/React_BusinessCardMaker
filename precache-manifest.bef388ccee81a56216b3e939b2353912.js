f448a539a9ac7a19b285572b9cb761e8e8ff93bc
박성언 <tjddjs90@naver.com>
2020-10-10 13:41:16 +0900

    Updates

--- precache-manifest.bef388ccee81a56216b3e939b2353912.js
+++ precache-manifest.bef388ccee81a56216b3e939b2353912.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d57ab55a9ae933f17793242747190c26",
+    "revision": "ef23dc86b2b8a8b10e0ac58c03debaf1",
     "url": "/React_BusinessCardMaker/index.html"
   },
   {
-    "revision": "e063ad351875599f600e",
+    "revision": "2cc427eeda651e5cefcf",
     "url": "/React_BusinessCardMaker/static/css/main.7d97f49f.chunk.css"
   },
   {
-    "revision": "becb6832af40d4513d51",
-    "url": "/React_BusinessCardMaker/static/js/2.24ee1f4c.chunk.js"
+    "revision": "3bbd77819037c9f1e9bf",
+    "url": "/React_BusinessCardMaker/static/js/2.5a0b4b36.chunk.js"
   },
   {
     "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
-    "url": "/React_BusinessCardMaker/static/js/2.24ee1f4c.chunk.js.LICENSE.txt"
+    "url": "/React_BusinessCardMaker/static/js/2.5a0b4b36.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e063ad351875599f600e",
-    "url": "/React_BusinessCardMaker/static/js/main.d777c99c.chunk.js"
+    "revision": "2cc427eeda651e5cefcf",
+    "url": "/React_BusinessCardMaker/static/js/main.22f22baf.chunk.js"
   },
   {
     "revision": "85391f147e046045ec76",
