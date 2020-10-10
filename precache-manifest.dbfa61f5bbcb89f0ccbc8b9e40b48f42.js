a29e5258a42c3c6c3175ec74ecd015353dedd8e1
박성언 <tjddjs90@naver.com>
2020-10-10 13:08:43 +0900

    Updates

---
+++ precache-manifest.dbfa61f5bbcb89f0ccbc8b9e40b48f42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7281ac9e97d14aefa1d8200d623889",
+    "url": "/React_BusinessCardMaker/index.html"
+  },
+  {
+    "revision": "1af667f4145a591056fb",
+    "url": "/React_BusinessCardMaker/static/css/main.7d97f49f.chunk.css"
+  },
+  {
+    "revision": "becb6832af40d4513d51",
+    "url": "/React_BusinessCardMaker/static/js/2.24ee1f4c.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/React_BusinessCardMaker/static/js/2.24ee1f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af667f4145a591056fb",
+    "url": "/React_BusinessCardMaker/static/js/main.7bb399b7.chunk.js"
+  },
+  {
+    "revision": "85391f147e046045ec76",
+    "url": "/React_BusinessCardMaker/static/js/runtime-main.2fdd80ab.js"
+  }
+]);
